8fd2f6d0bb4072e2339656ead828459fbfe0ac0f
misabelf <maria.fernandes@invillia.com>
2022-08-22 23:32:55 -0300

    aplica condicional de tempo de animação

--- sketch.js
+++ sketch.js
@@ -1,178 +1,180 @@
 function setup(){
-    createCanvas(2000, 2000, WEBGL);
-    frameRate(60);
-  }
+  createCanvas(2000, 2000, WEBGL);
+  frameRate(60);
+}
+
+function draw(){
+  background('gray');
+  orbitControl();
+  //setar eixos
+  strokeWeight(2);
+  stroke('red');
+  line(-600,0,0,600,0,0); //eixo x vermelho
+  stroke('green');
+  line(0,-600,0,0,600,0); // eixo y verde
+  stroke('blue');
+  line(0, 0, -800, 0, 0, 800); //eixo z azul
+  //marca os pontos A, B, C D, E, respectivamente
+  strokeWeight(10)
+  stroke('blue');
+  point(400,100,0);
+  stroke('red');
+  point(-100, 50/3, 500);
+  stroke('black');
+  point(0,0,600);
+  stroke('yellow');
+  point(100,-50/3,500);
+  stroke('green');
+  point(-400,-100,0);
   
-  function draw(){
-    background('gray');
-    orbitControl();
-    //setar eixos
-    strokeWeight(2);
-    stroke('red');
-    line(-600,0,0,600,0,0); //eixo x vermelho
-    stroke('green');
-    line(0,-600,0,0,600,0); // eixo y verde
-    stroke('blue');
-    line(0, 0, -800, 0, 0, 800); //eixo z azul
-    //marca os pontos A, B, C D, E, respectivamente
-    strokeWeight(10)
-    stroke('blue');
-    point(400,100,0);
-    stroke('red');
-    point(-100, 50/3, 500);
-    stroke('black');
-    point(0,0,600);
-    stroke('yellow');
-    point(100,-50/3,500);
-    stroke('green');
-    point(-400,-100,0);
-    
-    const rotX = -PI/2;
-    const rotY = -atan(1/6);
-    const w1 = PI/6; //velocidade ang em AB e DE
-    const w2 = PI/2; //vel ang em BD
-    const r1 = 500; //raio AB e DE
-    const r2 = 100; // raio BD
-    const t = millis() / 1000;
-    
-    /*
-    //circulo que passa pelo arco AB
-    push()
-    translate(400, 100 , 500);
-    rotateX(rotX)
-    rotateY(rotY)
-    stroke('purple')
-    strokeWeight(2)
-    circle(0,0,1000)
-    pop()
-    */
+  const rotX = -PI/2;
+  const rotY = -atan(1/6);
+  const w1 = PI/6; //velocidade ang em AB e DE
+  const w2 = PI/2; //vel ang em BD
+  const v1 = 250 * PI/3;
+  const v2 = 50 * PI;
+  //const r1 = 500; //raio AB e DE
+  //const r2 = 100; // raio BD
+  const t = millis() / 1000;
   
-    // rotacaoAB
-     push();
-    applyMatrix(
-           1,0,0,0,
-          0,1,0,0,
-          0,0,1,0,
-          400,100,500,1
-      );
-    applyMatrix(1,0,0,0,
-               0,cos(rotX) ,sin(rotX) ,0,
-               0, -sin(rotX), cos(rotX),0,
-              0,0,0,1
-      ); // rot em X
-    applyMatrix(cos(rotY),0,-sin(rotY),0,
-               0,1 ,0 ,0,
-               sin(rotY), 0, cos(rotY),0,
-              0,0,0,1
-      ); // rot Y
-     applyMatrix(cos(w1 * t),sin(w1 * t),0,0,
-                 -sin(w1 * t),  cos(w1 * t),0,0,
-                 0,0 ,1 ,0,  
-              0,0,0,1
-      );// rot Z
-    stroke('brown')
-    applyMatrix(
-           1,0,0,0,
-          0,1,0,0,
-          0,0,1,0,
-          0,500,0,1
-      );
-    sphere(20)
-    pop();
-    
-    /*
-    //circulo que passa pelo arco BD
-    push()
-    translate(0, 0 , 500);
-    rotateX(PI/2)
-    rotateY(-atan(1/6))
-    stroke('green')
-    strokeWeight(2)
-    circle(0,0,200)
-    pop()
-    */
+  /*
+  //circulo que passa pelo arco AB
+  push()
+  translate(400, 100 , 500);
+  rotateX(rotX)
+  rotateY(rotY)
+  stroke('purple')
+  strokeWeight(2)
+  circle(0,0,1000)
+  pop()
+  */
+
+  // rotacaoAB
+   push();
+  applyMatrix(
+         1,0,0,0,
+        0,1,0,0,
+        0,0,1,0,
+        400,100,500,1
+    );
+  applyMatrix(1,0,0,0,
+             0,cos(rotX) ,sin(rotX) ,0,
+             0, -sin(rotX), cos(rotX),0,
+            0,0,0,1
+    ); // rot em X
+  applyMatrix(cos(rotY),0,-sin(rotY),0,
+             0,1 ,0 ,0,
+             sin(rotY), 0, cos(rotY),0,
+            0,0,0,1
+    ); // rot Y
+   applyMatrix(cos(w1*t),sin(w1*t),0,0,
+               -sin(w1*t),  cos(w1*t),0,0,
+               0,0 ,1 ,0,  
+            0,0,0,1
+    );// rot Z
+  stroke('pink')
+  applyMatrix(
+         1,0,0,0,
+        0,1,0,0,
+        0,0,1,0,
+        0,500,0,1
+    );
+  sphere(20)
+  pop();
   
-    //rotacao BD
-     push();
-    applyMatrix(
-           1,0,0,0,
-          0,1,0,0,
-          0,0,1,0,
-          0,0,500,1
-      );
-    applyMatrix(1,0,0,0,
-               0,cos(rotX) ,sin(rotX) ,0,
-               0, -sin(rotX), cos(rotX),0,
-              0,0,0,1
-      ); // rot em X
-    applyMatrix(cos(rotY),0,sin(rotY),0,
-               0,1 ,0 ,0,
-               -sin(rotY), 0, cos(rotY),0,
-              0,0,0,1
-      ); // rot Y
-     applyMatrix(cos(w2 * t),sin(w2 * t),0,0,
-                 -sin(w2 * t),  cos(w2 * t),0,0,
-                 0,0 ,1 ,0,  
-              0,0,0,1
-      );// rot Z
-     
-    //esfera 
-    stroke('brown')
-    applyMatrix(
-           1,0,0,0,
-          0,1,0,0,
-          0,0,1,0,
-          -100,0,0,1
-      );
-    sphere(20)
-    pop();
-    
-    /*
-     // circulo que passa pelo arco DE
-    push()
-    translate(-400, -100 , 500);
-    rotateX(-PI/2)
-    rotateY(atan(-1/6))
-    stroke('rgb(209,86,107)')
-    strokeWeight(2)
-    circle(0,0,1000)
-    pop()
-    */
-    //rotaçao DE
-    push();
-    applyMatrix(
-           1,0,0,0,
-          0,1,0,0,
-          0,0,1,0,
-          -400,-100,500,1
-      );
-    applyMatrix(1,0,0,0,
-               0,cos(rotX) ,sin(rotX) ,0,
-               0, -sin(rotX), cos(rotX),0,
-              0,0,0,1
-      ); // rot em X
-    
-    applyMatrix(cos(rotY),0,-sin(rotY),0,
-               0,1 ,0 ,0,
-               sin(rotY), 0, cos(rotY),0,
-              0,0,0,1
-      ); // rot Y
-    
-     applyMatrix(cos(w1 * t),sin(w1 * t),0,0,
-                 -sin(w1 * t),  cos(w1 * t),0,0,
-                 0,0 ,1 ,0,  
-              0,0,0,1
-      );// rot Z
-    
-    //esfera 
-    stroke('brown')
-    applyMatrix(
-           1,0,0,0,
-          0,1,0,0,
-          0,0,1,0,
-          500,0,0,1
-      );
-    sphere(20)
-    pop();
-    
-  }
+  /*
+  //circulo que passa pelo arco BD
+  push()
+  translate(0, 0 , 500);
+  rotateX(PI/2)
+  rotateY(-atan(1/6))
+  stroke('green')
+  strokeWeight(2)
+  circle(0,0,200)
+  pop()
+  */
+
+  //rotacao BD
+   push();
+  applyMatrix(
+         1,0,0,0,
+        0,1,0,0,
+        0,0,1,0,
+        0,0,500,1
+    );
+  applyMatrix(1,0,0,0,
+             0,cos(rotX) ,sin(rotX) ,0,
+             0, -sin(rotX), cos(rotX),0,
+            0,0,0,1
+    ); // rot em X
+  applyMatrix(cos(rotY),0,sin(rotY),0,
+             0,1 ,0 ,0,
+             -sin(rotY), 0, cos(rotY),0,
+            0,0,0,1
+    ); // rot Y
+   applyMatrix(cos(w2*t),sin(w2*t),0,0,
+               -sin(w2*t),  cos(w2*t),0,0,
+               0,0 ,1 ,0,  
+            0,0,0,1
+    );// rot Z
+   
+  //esfera 
+  stroke('purple')
+  applyMatrix(
+         1,0,0,0,
+        0,1,0,0,
+        0,0,1,0,
+        -100,0,0,1
+    );
+  sphere(20)
+  pop();
+  
+  /*
+   // circulo que passa pelo arco DE
+  push()
+  translate(-400, -100 , 500);
+  rotateX(-PI/2)
+  rotateY(atan(-1/6))
+  stroke('rgb(209,86,107)')
+  strokeWeight(2)
+  circle(0,0,1000)
+  pop()
+  */
+  //rotaçao DE
+  push();
+  applyMatrix(
+         1,0,0,0,
+        0,1,0,0,
+        0,0,1,0,
+        -400,-100,500,1
+    );
+  applyMatrix(1,0,0,0,
+             0,cos(rotX) ,sin(rotX) ,0,
+             0, -sin(rotX), cos(rotX),0,
+            0,0,0,1
+    ); // rot em X
+  
+  applyMatrix(cos(rotY),0,-sin(rotY),0,
+             0,1 ,0 ,0,
+             sin(rotY), 0, cos(rotY),0,
+            0,0,0,1
+    ); // rot Y
+  
+   applyMatrix(cos(w1*t),sin(w1*t),0,0,
+               -sin(w1*t),  cos(w1*t),0,0,
+               0,0 ,1 ,0,  
+            0,0,0,1
+    );// rot Z
+  
+  //esfera 
+  stroke('orange')
+  applyMatrix(
+         1,0,0,0,
+        0,1,0,0,
+        0,0,1,0,
+        500,0,0,1
+    );
+  sphere(20)
+  pop();
+  
+}
